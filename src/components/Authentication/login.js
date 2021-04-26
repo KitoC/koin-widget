0efc9e773543d04d2d04171f7cfc152e74de1412
Kito Clark <kito_clark@flightcentre.com>
2021-04-26 10:15:52 +1000

    added redux toolkit

---
+++ src/components/Authentication/login.js
@@ -0,0 +1,15 @@
+// const items = [{ label: "Help" }];
+
+// const MenuItem = ({ label }) => <div style={{ height: 100 }}>{label}</div>;
+
+const Menu = () => {
+  return (
+    <div>
+      {/* {items.map((item) => (
+        <MenuItem label={item.label} />
+      ))} */}
+    </div>
+  );
+};
+
+export default Menu;
